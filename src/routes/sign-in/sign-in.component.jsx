09c20a7259d4cd2e2be847d90c4157924191efe9
OlegI <aka.sting@gmail.com>
2022-12-15 00:03:43 +0300

    step 9

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,39 +1,19 @@
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import {
-  auth,
   signInWithGooglePopup,
-  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
-  useEffect(() => {
-    async function processRedirect() {
-      const response = await getRedirectResult(auth);
-      if (response) {
-        const userDocRef = await createUserDocumentFromAuth(response.user);
-      }
-    }
-    processRedirect();
-  }, []);
-
   const signInGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
-  };
-  const signInGoogleUserRedirect = async () => {
-    const { user } = await signInWithGoogleRedirect();
-    //const userDocRef = await createUserDocumentFromAuth(user);
-    console.log(user);
   };
   return (
     <div>
       <h1> Sign In Page</h1>
       <button onClick={signInGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={signInGoogleUserRedirect}>
-        Sign in with Google Redirect
-      </button>
+      <SignUpForm></SignUpForm>
     </div>
   );
 };
