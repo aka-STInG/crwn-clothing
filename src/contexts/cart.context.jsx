1fe0428b0d845fe7b7524b1c19636cb57db912a4
OlegI <aka.sting@gmail.com>
2022-12-15 04:04:10 +0300

    step 15

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,9 +1,32 @@
 import { createContext, useState } from "react";
 
-export const CartContext = createContext({});
+const addCartItem = (cartItems, productToAdd) => {
+  const item = cartItems.find((i) => i.id === productToAdd.id);
+  if (item) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  } else {
+    return [...cartItems, { ...productToAdd, quantity: 1 }];
+  }
+};
+
+export const CartContext = createContext({
+  isCartOpen: false,
+  setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+});
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
